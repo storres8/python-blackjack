04013c9596422e54a5f2fc1fc375495955ae268c
storres8 <stevetorres7562@gmail.com>
2018-10-20 21:13:07 -0400

    added a deck class with a shuffle method

--- index.py
+++ index.py
@@ -12,4 +12,24 @@ class Card():
         self.rank = rank
 
     def __str__(self):
-        print(f"{self.rank} of {self.suit}")
+        return f"{self.rank} of {self.suit}"
+
+class Deck():
+    def __init__(self):
+        self.deck=[]
+        for suit in suits:
+            for rank in ranks:
+                self.deck.append(Card(suit,rank))
+
+    def __str__(self):
+        deck_comp = ''
+        for card in self.deck:
+            deck_comp += '\n '+card.__str__()
+        return 'The deck has:' + deck_comp
+
+    def shuffle(self):
+        random.shuffle(self.deck)
+
+    def deal(self):
+        single_card = self.deck.pop()
+        return single_card
