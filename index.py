48fa4ccf7e5c0e93d0dc9026ba49fe87e9bdf4cb
storres8 <stevetorres7562@gmail.com>
2018-10-22 17:16:03 -0400

    made a class to handle for Ace logic

--- index.py
+++ index.py
@@ -33,3 +33,23 @@ class Deck():
     def deal(self):
         single_card = self.deck.pop()
         return single_card
+
+class Hand():
+    def __init__(self):
+        self.cards= []
+        self.value= 0
+        self.aces = 0
+
+    def add_card(self, card):
+        self.cards.append(card)
+        self.value += values[card.rank]
+
+        #tracks aces
+        if card.rank == "Ace":
+            self.aces += 1
+
+    def adjust_for_ace(self):
+        while self.value > 21 and self.aces:
+            # if 0 number is treated as false so function will not evaluate
+            self.value -= 10
+            self.aces -= 1
